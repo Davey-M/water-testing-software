38580f22697b556f667c70e9be8394765fe1a51f
Davey-M <davey.meuer@gmail.com>
2022-05-17 11:31:47 -0500

    create component structure

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// component imports
+import Nav from '../Nav/Nav';
+import Home from '../Home/Home';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to=''>Home</Link>
-        <Link to='about'>About</Link>
-      </nav>
+      <Nav />
       <div>
         <Routes>
-          <Route path='' element={<h1>Hello World</h1>} />
+          <Route path='' element={<Home />} />
           <Route path='about' element={<h1>About</h1>} />
         </Routes>
       </div>

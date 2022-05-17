b236cff237a2e491da433ff937cd58c7cc7eaefa
Davey-M <davey.meuer@gmail.com>
2022-05-17 17:33:17 -0500

    make clear what page you are viewing

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,22 +1,43 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // style imports
 import styles from './Nav.module.css';
 
 function Nav() {
+  const [chosen, setChosen] = useState(1);
+
+  const choose = (num: number, chose: number) => {
+    if (num === chose) return styles.link + ' ' + styles.chosen;
+
+    return styles.link;
+  };
+
   return (
     <>
       <nav className={styles.container}>
-        <Link className={styles.link} to=''>
+        <Link className={choose(1, chosen)} onClick={(e) => setChosen(1)} to=''>
           Home
         </Link>
-        <Link className={styles.link} to='test'>
+        <Link
+          className={choose(2, chosen)}
+          onClick={(e) => setChosen(2)}
+          to='test'
+        >
           Test Water
         </Link>
-        <Link className={styles.link} to='chemicals'>
+        <Link
+          className={choose(3, chosen)}
+          onClick={(e) => setChosen(3)}
+          to='chemicals'
+        >
           Chemicals
         </Link>
-        <Link className={styles.link} to='profiles'>
+        <Link
+          className={choose(4, chosen)}
+          onClick={(e) => setChosen(4)}
+          to='profiles'
+        >
           Chemical Profiles
         </Link>
       </nav>

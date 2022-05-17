e7924fc6a583898d81f75d682f80e1580a7afe0e
Davey-M <davey.meuer@gmail.com>
2022-05-17 11:24:44 -0500

    browser router works in tauri

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,19 @@
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <nav>
+        <Link to=''>Home</Link>
+        <Link to='about'>About</Link>
+      </nav>
+      <div>
+        <Routes>
+          <Route path='' element={<h1>Hello World</h1>} />
+          <Route path='about' element={<h1>About</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
